ee99e392b324a735ded5a58d75e584d196335eee
Adisdzaky <adisdzakyrivai@gmail.com>
2025-01-21 11:03:19 +0700

    Update index.js

--- index.js
+++ index.js
@@ -10,5 +10,11 @@ app.get('/api/send', sendHandler);
 
 app.get('/api/getcode', getcodeHandler);
 
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ error: 'Internal Server Error' });
+});
+
 // Konfigurasi untuk Vercel
 module.exports = app;
